11ac3af446da771f7c0807702cbe042156e10e33
Adam Brentnall <a.brentnall@qmul.ac.uk>
2020-04-23 22:12:36 +0100

    Update analysis with new charts showing number diagnosed, plus route breakdown by age for stomach type

--- analysis/200416-anal-gistphe.r
+++ analysis/200416-anal-gistphe.r
@@ -26,7 +26,7 @@ mydta3[,4:41]<-apply(mydta3[,4:41],2,function(ind) as.double(as.character(ind)))
 
 mydta3$Year <- as.integer(as.character(mydta3$Year))
 
-mydta4<- gather(mydta3[,c(1,2,, "key", "route", Two.Week.Wait.percentage, GP.Referral.percentage, Other.Outpatient.percentage, Inpatient.Elective.percentage, Emergency.Presentation.percentage, DCO.percentage, Unknown.Route.percentage, Route.not.classified.percentage)
+mydta4<- gather(mydta3, "key", "route", Two.Week.Wait.percentage, GP.Referral.percentage, Other.Outpatient.percentage, Inpatient.Elective.percentage, Emergency.Presentation.percentage, DCO.percentage, Unknown.Route.percentage, Route.not.classified.percentage)
 
 mydta4$key  <- substr(mydta4$key, 1, nchar(mydta4$key)-11)
 
@@ -37,10 +37,22 @@ myordkey  <- rev(c("GP.Referral","Emergency.Presentation", "Other.Outpatient", "
 mydta4<-mydta4[order(match(mydta4$key, myordkey)),]
 
 mydta4$key <- factor(mydta4$key, levels=myordkey) 
-                       
+
+## number not rate
+mydta5<- gather(mydta3, "key", "route", Two.Week.Wait, GP.Referral, Other.Outpatient, Inpatient.Elective, Emergency.Presentation, DCO, Unknown.Route, Route.not.classified)
+
+unique(mydta5$key)
+
+myordkey  <- rev(c("GP.Referral","Emergency.Presentation", "Other.Outpatient", "Two.Week.Wait", "Inpatient.Elective", "DCO",  "Route.not.classified"  , "Unknown.Route"))
+
+mydta5<-mydta5[order(match(mydta5$key, myordkey)),]
+
+mydta5$key <- factor(mydta5$key, levels=myordkey) 
+
 
 
 ## plot 1 - incidence rate by year
+
 
 ggplot(data = mydta3[mydta3$Age=="All ages",], aes(x = Year, y = Incidence.Rate, group=Tumour.Type.3)) +
     geom_line(aes(color=Tumour.Type.3, linetype=Tumour.Type.3), size=1) +
@@ -51,12 +63,27 @@ ggplot(data = mydta3[mydta3$Age=="All ages",], aes(x = Year, y = Incidence.Rate,
          x = "Year", y = "Incidence rate (per 100,000 population)") +
      theme(legend.title=element_blank())
 
+##plot 2 - number by year
+
+t.inc<- mydta3$Age=="All ages" & mydta3$Tumour.Type.3!="All"
+ggplot(data = mydta3[t.inc,], aes(x = Year, y = Incidence)) +
+    geom_col(aes(fill=Tumour.Type.3))+
+    labs(title = "Number GIST diagnosed by year",
+         x = "Year", y = "Number") +
+     theme(legend.title=element_blank())
+
+
 ##plot 2 - route by year
 
 p <- ggplot(mydta4[mydta4$Age=="All ages" & mydta4$Tumour.Type.3=="All",], aes(x = Year, y = route))+
   geom_col(aes(fill = key), width = 0.7) +
     labs(title = "Routes by year",
          x = "Year", y = "Percentage (%)") 
+
+
+
+
+
 
 
 ## plot 3 - Stomach by age group and year
@@ -69,7 +96,33 @@ bp <- ggplot(data = mydta3[mydta3$Tumour.Type.3=="Stomach" & mydta3$Age!="All ag
          x = "Year", y = "Incidence rate (per 100,000 population)") +
     theme(legend.title=element_blank())
 
-bp
+
+bp <- ggplot(data = mydta3[mydta3$Tumour.Type.3=="Stomach" & mydta3$Age!="All ages",], aes(x = Year, y = Incidence, group=Age)) +
+    geom_col(aes(fill=Age)) +
+    labs(title = "GIST Stomach Tumour Number by year",
+         x = "Year", y = "Number diagnosed") +
+    theme(legend.title=element_blank())
+
+
+
+
+##plot 3 - stomach route by age
+
+p <- ggplot(mydta5[mydta5$Tumour.Type.3=="Stomach" & (mydta5$Age!="All ages"),], aes(x=Age,y=route))+
+    geom_col(aes(fill = key), width = 0.7) +
+    labs(title = "Stomatch routes (2013-16) by age group",
+         x = "Age group", y = "Number (total 2013-16)") 
+
+
+
+p <- ggplot(mydta5[mydta5$Tumour.Type.3=="Stomach",], aes(Age))+
+    geom_bar(position="fill",aes(y=route,fill = key), width = 0.7, stat="identity") +
+    scale_y_continuous(labels = scales::percent) +
+    labs(title = "Stomatch routes (2013-16) by age group",
+         x = "Age group", y = "Percentage (%)") 
+
+
+
 
 
 
